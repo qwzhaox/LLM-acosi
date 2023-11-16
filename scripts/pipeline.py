08596764f4992c3cbc01e130b79ca0ce736726ea
Wenzhao Qiu <qwzhao@gl-login1.arc-ts.umich.edu>
2023-11-16 00:03:57 -0500

    start script

---
+++ scripts/pipeline.py
@@ -0,0 +1,50 @@
+import argparse
+import json
+import torch
+from transformers import pipeline, AutoTokenizer
+from tqdm import tqdm
+
+device = 0 if torch.cuda.is_available() else -1
+
+
+def dolly_15k_format_prompt(prompt):
+    instruction_key = "### Instruction:"
+    response_key = "### Response:"
+    intro_blurb = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+    prompt_for_generation_format = """{intro}
+                                      {instruction_key}
+                                      {instruction}
+                                      {response_key}
+                                    """.format(
+        intro=intro_blurb,
+        instruction_key=instruction_key,
+        instruction=prompt,
+        response_key=response_key,
+    )
+    return prompt_for_generation_format, response_key
+
+
+def run_pipeline(args, prompt):
+    # Initialize the pipeline with the specified model, and set the device
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
+
+    model_pipe = pipeline(
+        args.task,
+        model=args.model_name,
+        device_map="auto",
+        tokenizer=args.tokenizer,
+        trust_remote_code=args.remote,
+    )
+
+    with open(args.dataset_file, "r") as f:
+        dataset = f.readlines()
+
+    formatted_prompt, response_key = dolly_15k_format_prompt(prompt)
+
+    output = []
+
+    for data in tqdm(dataset, desc="Processing", unit="item"):
+        pass
+
+    with open(args.output_file, "w") as f:
+        pass
