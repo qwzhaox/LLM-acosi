52ccfda69dc49f30dfdddfe00c9c733e84e5e776
Wenzhao Qiu <qwzhao@gl-login1.arc-ts.umich.edu>
2023-11-22 17:05:48 -0500

    modified acos extend logic

--- scripts/acos_extend.py
+++ scripts/acos_extend.py
@@ -3,16 +3,16 @@ from pprint import pprint
 from pipeline import run_pipeline
 from utils import get_args, format_output
 
+OPINION_IDX = 3
+
 
 def get_ACOS_extend_prompt():
     prompt = """
-    Given an online customer review and its corresponding ACOS quadruples, 
-    convert the ACOS (Aspect-Category-Opinion-Sentiment) quadruples to ACOSI (Aspect-Category-Opinion-Sentiment-Implicit/Explicit) quintuples by\n
-    1) identifying and labeling the corresponding opinion span for implicit opinions (labeled NULL in ACOS).\n
-    2) adding an the implicit indicator term to each quadruple (making it a quintuple) that indicates whether the opinion is implicit or explicit (indirect/direct).\n\n
+    Given an online customer review and its corresponding ACOS (Aspect-Category-Opinion-Sentimeent) quadruples, 
+    identify and label the corresponding opinion span for implicit opinions (labeled NULL in ACOS).\n\n
     """
 
-    response_head = "ACOSI quintuples:"
+    response_head = "Opinion spans:"
 
     example1 = f"""
     Example 1:\n\n
@@ -24,9 +24,7 @@ def get_ACOS_extend_prompt():
                       \n
 
     Response:\n
-    {response_head} [(Aspect: "surface", Category: design, Sentiment: positive, Opinion: "smooth", Implicit/Explicit: direct),
-                       (Aspect: NULL, Category: design, Sentiment: positive, Opinion: "nice", Implicit/Explicit: direct),
-                       (Aspect: "apps", Category: software, Sentiment: negative, Opinion: "apps take seconds to respond", Implicit/Explicit: indirect)]
+    {response_head} [Opinion: "smooth", Opinion: "nice", Implicit Opinion: "apps take seconds to respond"]
                        \n\n
     """
 
@@ -34,15 +32,13 @@ def get_ACOS_extend_prompt():
     Example 2:\n\n
 
     Review: with the theater 2 blocks away we had a delicious meal in a beautiful room .\n
-    ACOS quadruples: [(Aspect: "meal", Category: food#quality, Sentiment: positive, Opinion: "delicious"), 
-                      (Aspect: "room", Category: ambience#general, Sentiment: positive, Opinion: "beautiful"), 
-                      (Aspect: NULL, Category: location#general, Sentiment: positive, Opinion: NULL)]
+    ACOS quadruples: [(Aspect: "meal", Category: food#quality, Sentiment: positive, Opinion: "delicious"),
+                      (Aspect: NULL, Category: location#general, Sentiment: positive, Opinion: NULL),
+                      (Aspect: "room", Category: ambience#general, Sentiment: positive, Opinion: "beautiful")]
                       \n
     
     Response:\n
-    {response_head} [(Aspect: "meal", Category: food#quality, Sentiment: positive, Opinion: "delicious", Implicit/Explicit: direct),
-                       (Aspect: "room", Category: ambience#general, Sentiment: positive, Opinion: "beautiful", Implicit/Explicit: direct),
-                       (Aspect: NULL, Category: location#general, Sentiment: positive, Opinion: "theater 2 blocks away", Implicit/Explicit: indirect)]
+    {response_head} [Opinion: "delicious", Implicit Opinion: "theater 2 blocks away", Opinion: "beautiful"]
                        \n\n
     """
 
@@ -53,12 +49,38 @@ def get_ACOS_extend_prompt():
 
 def main(args):
     prompt, examples, response_head = get_ACOS_extend_prompt()
-    output, response_key = run_pipeline(args, prompt, examples, absa_task="acos-extend")
+    opinion_spans, response_key = run_pipeline(
+        args, prompt, examples, absa_task="acos-extend"
+    )
     # with open(args.output_file, "r") as f:
     #     output = load(f)
     # response_key = "#### Response:"
-    formatted_output = format_output(output, response_key, response_head)
+    formatted_output = format_output(opinion_spans, response_key, response_head)
     pprint(formatted_output)
+
+    with open(args.dataset_file, "r") as f:
+        dataset = f.readlines()
+
+    assert len(dataset) == len(formatted_output)
+
+    acos_annotations = [eval(x.split("####")[1]) for x in dataset]
+    acosi_annotations = []
+
+    for quadruples, opinion_spans in zip(acos_annotations, formatted_output):
+        cur_acosi_annotation = []
+        if len(quadruples) == len(opinion_spans):
+            for quad, opinion_span in zip(quadruples, opinion_spans):
+                if quad[OPINION_IDX] == "NULL":
+                    quad[OPINION_IDX] = opinion_span
+                    quint = tuple(quad.append("indirect"))
+                else:
+                    quint = tuple(quad.append("direct"))
+                cur_acosi_annotation.append(quint)
+
+        acosi_annotations.append(cur_acosi_annotation)
+
+    pprint(acosi_annotations)
+
     with open(args.output_file, "wb") as f:
         dump(formatted_output, f)
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -31,11 +31,16 @@ def get_args():
     return args
 
 
+def flatten_output(output):
+    return list(chain(*output))
+
+
 def remove_tags(text):
     return (
         text.replace("Aspect: ", "")
         .replace("Category: ", "")
         .replace("Sentiment: ", "")
+        .replace("Implicit Opinion: ", "")
         .replace("Opinion: ", "")
         .replace("Implicit/Explicit: ", "")
     )
@@ -52,37 +57,56 @@ def add_quotations(text):
     )
 
 
+def clean_output(out, response_key, response_head):
+    prediction = out["generated_text"].strip()
+
+    if response_key in prediction:
+        prediction = prediction.split(response_key)[1].strip()
+    if response_head in prediction:
+        prediction = prediction.split(response_head)[1].strip()
+
+    return prediction
+
+
+def extract_list_str(prediction):
+    if "[" in prediction and "]" in prediction:
+        prediction = prediction[prediction.find("[") : prediction.find("]") + 1]
+    elif "[" in prediction:
+        prediction = prediction[prediction.find("[") : prediction.rfind(")") + 1] + "]"
+    else:
+        return "", False
+    return prediction, True
+
+
+def format_list_str(prediction):
+    prediction = remove_tags(prediction)
+    prediction = add_quotations(prediction)
+    prediction = prediction.lower()
+    return prediction
+
+
+def eval_list_str(prediction):
+    try:
+        formatted_tuples = eval(prediction)
+    except Exception as e:
+        print(e)
+        print("Invalid output: ", prediction, "\n")
+        return []
+    return formatted_tuples
+
+
 def format_output(output, response_key, response_head):
-    output = list(chain(*output))
+    output = flatten_output(output)
     formatted_output = []
     for out in output:
-        prediction = out["generated_text"].strip()
-
-        if response_key in prediction:
-            prediction = prediction.split(response_key)[1].strip()
-        if response_head in prediction:
-            prediction = prediction.split(response_head)[1].strip()
-
-        if "[" in prediction and "]" in prediction:
-            prediction = prediction[prediction.find("[") : prediction.find("]") + 1]
-        elif "[" in prediction:
-            prediction = (
-                prediction[prediction.find("[") : prediction.rfind(")") + 1] + "]"
-            )
-        else:
+        prediction = clean_output(out, response_key, response_head)
+        prediction, valid = extract_list_str(prediction)
+        if not valid:
             formatted_output.append([])
             continue
 
-        prediction = remove_tags(prediction)
-        prediction = add_quotations(prediction)
-        prediction = prediction.lower()
-
-        try:
-            formatted_tuple = eval(prediction)
-            formatted_output.append(formatted_tuple)
-        except Exception as e:
-            print(e)
-            print("SKIPPING ANNOTATION")
-            formatted_output.append([])
+        prediction = format_list_str(prediction)
+        formatted_tuples = eval_list_str(prediction)
+        formatted_output.append(formatted_tuples)
 
     return formatted_output
