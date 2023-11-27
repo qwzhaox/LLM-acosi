18996e02176c40d2b69b530c81968e4aa9cc284a
HaitongC <haitongchen116@gmail.com>
2023-11-27 15:43:30 -0500

    clean up

--- scripts/gpt4.py
+++ scripts/gpt4.py
@@ -6,7 +6,6 @@ from pickle import dump
 from pipeline import run_pipeline, get_formatted_annotations, alpaca_format_prompt
 from utils import get_file_path, format_output, get_args, dump_output
 from acos_extract import get_ACOS_extract_prompt
-from acos_extend import get_ACOS_extend_prompt, get_ACOS_annotations, get_ACOSI_annotations
 from acosi_extract import get_ACOSI_extract_prompt
 from openai import OpenAI
 
@@ -28,8 +27,6 @@ def main(args):
             prompt, examples, response_head = get_ACOS_extract_prompt("laptop")
         else:
             raise ValueError("Invalid dataset domain.")
-    elif absa_task == "acos-extend":
-        prompt, examples, response_head = get_ACOS_extend_prompt()
     elif absa_task == "acosi-extract":
         prompt, examples, response_head = get_ACOSI_extract_prompt()
     else:
@@ -80,9 +77,6 @@ def main(args):
     # format for different tasks
     if absa_task == "acos-extract":
         formatted_output = [[quint[:-1] for quint in quints] for quints in formatted_output]
-    elif absa_task == "acos-extend":
-        acos_annotations = get_ACOS_annotations(len(formatted_output))
-        formatted_output = get_ACOSI_annotations(acos_annotations, formatted_output)
 
     dump_output(output_file, formatted_output)
     
